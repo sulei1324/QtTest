6b47df587a4c929889f1c01b197ae4232ab67564
sulei1324 <651343980@qq.com>
2015-07-23 22:38:03 +0800

    ok

--- QtTest/QtTest.cpp
+++ QtTest/QtTest.cpp
@@ -1,11 +1,13 @@
 // QtTest.cpp : 定义控制台应用程序的入口点。
 //
-
 #include "stdafx.h"
+#include <QApplication>
+#include "finddialog.h"
 
-
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char *argv[])
 {
-	return 0;
+        QApplication app(argc, argv);
+        FindDialog *dialog = new FindDialog;
+        dialog->show();
+        return app.exec();
 }
-
